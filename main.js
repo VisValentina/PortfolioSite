69e4fad587ac6e10550286d648946bb5eccf682a
Armand <apaquino@gmail.com>
2015-12-14 13:43:31 -0800

    add project mouseover/out functionality

--- main.js
+++ main.js
@@ -7,6 +7,46 @@ $(document).ready(function() {
 		showActiveTooltip: true,
 	});
 
+	$('#FWName').mouseover(function(){
+		$('#FWDesc').css('display', 'inline');
+	});
+
+	$('#FWName').mouseout(function(){
+		$('#FWDesc').css('display', 'none');
+	});
+
+	$('#MMName').mouseover(function(){
+		$('#MMDesc').css('display', 'inline');
+	});
+
+	$('#MMName').mouseout(function(){
+		$('#MMDesc').css('display', 'none');
+	});
+
+	$('#ZName').mouseover(function(){
+		$('#ZDesc').css('display', 'inline');
+	});
+
+	$('#ZName').mouseout(function(){
+		$('#ZDesc').css('display', 'none');
+	});
+
+	$('#FSName').mouseover(function(){
+		$('#FSDesc').css('display', 'inline');
+	});
+
+	$('#FSName').mouseout(function(){
+		$('#FSDesc').css('display', 'none');
+	});
+
+	$('#RName').mouseover(function(){
+		$('#RDesc').css('display', 'inline');
+	});
+
+	$('#RName').mouseout(function(){
+		$('#RDesc').css('display', 'none');
+	});
+
 });
 
  //-- End
