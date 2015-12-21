c0460423bb6a0334e381c9a353634eeb3a88d61a
Patty Pherson <Patty@iMac.local>
2015-12-21 11:00:02 -0800

    Add menu toggle with animation

--- MakeMe/makeme_main.js
+++ MakeMe/makeme_main.js
@@ -120,6 +120,29 @@ $(document).ready(function() {
 		e.stopPropagation();
 		$("#hoverNavLineWorks").animate({"width":"7em"}, 140);
 	});
+
+
+	//HAMBURGER
+	(function() {
+	  "use strict";
+	  var toggles = document.querySelectorAll(".c-hamburger");
+
+	  for (var i = toggles.length - 1; i >= 0; i--) {
+	    var toggle = toggles[i];
+	    toggleHandler(toggle);
+	  };
+
+	  function toggleHandler(toggle) {
+	    toggle.addEventListener( "click", function(e) {
+	      e.preventDefault();
+	      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+	    });
+	  }
+
+	})();
+	//HAMBURGER
 });
 
  //-- End
+
+
