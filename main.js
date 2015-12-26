630009938813193109ecda4759ae5041ebfa371a
Patty Pherson <Patty@iMac.local>
2015-12-25 16:45:18 -0800

    Remove filter functionality from works page

--- main.js
+++ main.js
@@ -17,9 +17,6 @@ $(document).ready(function() {
 			case 1:
 				console.log("back to first page");
 				$("#camel_fixed").fadeIn(8000);
-				// $(".camel_drawing").fadeIn(15000);
-				// $(".handPiece").fadeIn(15000);
-
 
 				$(".colorHider").hide();
 				$(".sliderToggle").hide();
@@ -112,21 +109,21 @@ $(document).ready(function() {
 		$("#hoverNavLine").animate({"width":currentIndexLength}, 140);
 	});
 
-	$('#FWName').mouseover(function(){
+	$('#FogWindow').mouseover(function(){
 		$('#FWDesc').css('display', 'inline');
 		$('#javascriptSelected').css('color', '#a8a8a8');
 		$('#artdirectionSelected').css('color', '#a8a8a8');
 		$('#css3Selected').css('color', '#a8a8a8');
 	});
 
-	$('#FWName').mouseout(function(){
+	$('#FogWindow').mouseout(function(){
 		$('#FWDesc').css('display', 'none');
 		$('#javascriptSelected').css('color', '#f0f0f0');
 		$('#artdirectionSelected').css('color', '#f0f0f0');
 		$('#css3Selected').css('color', '#f0f0f0');
 	});
 
-	$('#MMName').mouseover(function(){
+	$('#MakeMe').mouseover(function(){
 		$('#MMDesc').css('display', 'inline');
 		$('#uiuxSelected').css('color', '#a8a8a8');
 		$('#reactNativeSelected').css('color', '#a8a8a8');
@@ -134,7 +131,7 @@ $(document).ready(function() {
 		$('#javscriptSelected').css('color', '#a8a8a8');
 	});
 
-	$('#MMName').mouseout(function(){
+	$('#MakeMe').mouseout(function(){
 		$('#MMDesc').css('display', 'none');
 		$('#uiuxSelected').css('color', '#f0f0f0');
 		$('#reactNativeSelected').css('color', '#f0f0f0');
@@ -142,33 +139,33 @@ $(document).ready(function() {
 		$('#javscriptSelected').css('color', '#f0f0f0');
 	});
 
-	$('#ZName').mouseover(function(){
+	$('#Zween').mouseover(function(){
 		$('#ZDesc').css('display', 'inline');
 		$('#brandingSelected').css('color', '#a8a8a8');
 		$('#artdirectionSelected').css('color', '#a8a8a8');
 	});
 
-	$('#ZName').mouseout(function(){
+	$('#Zween').mouseout(function(){
 		$('#ZDesc').css('display', 'none');
 		$('#brandingSelected').css('color', '#f0f0f0');
 		$('#artdirectionSelected').css('color', '#f0f0f0');
 	});
 
-	$('#FSName').mouseover(function(){
+	$('#ForSyria').mouseover(function(){
 		$('#FSDesc').css('display', 'inline');
 		$('#javascriptSelected').css('color', '#a8a8a8');
 		$('#css3Selected').css('color', '#a8a8a8');
 		$('#artdirectionSelected').css('color', '#a8a8a8');
 	});
 
-	$('#FSName').mouseout(function(){
+	$('#ForSyria').mouseout(function(){
 		$('#FSDesc').css('display', 'none');
 		$('#javascriptSelected').css('color', '#f0f0f0');
 		$('#css3Selected').css('color', '#f0f0f0');
 		$('#artdirectionSelected').css('color', '#f0f0f0');
 	});
 
-	$('#RName').mouseover(function(){
+	$('#Runside').mouseover(function(){
 		$('#RDesc').css('display', 'inline');
 		$('#d3Selected').css('color', '#a8a8a8');
 		$('#reactSelected').css('color', '#a8a8a8');
@@ -176,7 +173,7 @@ $(document).ready(function() {
 		$('#javsacriptSelected').css('color', '#a8a8a8');
 	});
 
-	$('#RName').mouseout(function(){
+	$('#Runside').mouseout(function(){
 		$('#RDesc').css('display', 'none');
 		$('#d3Selected').css('color', '#f0f0f0');
 		$('#reactSelected').css('color', '#f0f0f0');
@@ -186,21 +183,48 @@ $(document).ready(function() {
 
 	// Click on tag will filter the project titles pertaining to it
 	// GLOBALS
-	var emptyCheckbox = 'rgb(240, 240, 240)',
-		fullCheckbox = '#a8a8a8';
+	// var emptyCheckbox = 'rgb(240, 240, 240)',
+	// 	fullCheckbox = '#a8a8a8';
 
-	$('#brandingClick').click(function(){
-		var label = $('#brandingClick'),
-			checkbox = $('#brandingSelected');
+	// $('#brandingClick').click(function(){
+	// 		checkbox = $('#brandingSelected');
 
-		if(checkbox.css('color') === emptyCheckbox) {
-			checkbox.css('color', fullCheckbox);
-			$('.FogWindow').remove();
-		} else {
-			checkbox.css('color', emptyCheckbox);
-			$('#preFogWindow').append( "<div class='FogWindow'><span id='FWName' class='subHeader projectTitle'><a href='#''>FogWindow</a></span><span id='FWDesc' class='subHeader projectTitle workDescription'>Fog up your screen</span><br></div>");
-		}
-	});
+	// 	if(checkbox.css('color') === emptyCheckbox) {
+	// 		checkbox.css('color', fullCheckbox);
+	// 		$('.FogWindow').remove();
+	// 	} else {
+	// 		checkbox.css('color', emptyCheckbox);
+	// 		$('#preFogWindow').append( "<div class='FogWindow'><span id='FWName' class='subHeader projectTitle'><a href='#''>FogWindow</a></span><span id='FWDesc' class='subHeader projectTitle workDescription'>Fog up your screen</span><br></div>");
+	// 	}
+	// });
+
+	// $('#uiuxClick').click(function(){
+	// 		checkbox = $('#uiuxSelected');
+
+	// 	if(checkbox.css('color') === emptyCheckbox) {
+	// 		checkbox.css('color', fullCheckbox);
+	// 		$('.ForSyria').remove();
+	// 		$('.Zween').remove();
+	// 	} else {
+	// 		checkbox.css('color', emptyCheckbox);
+	// 		$('#preForSyria').append( "<div class='ForSyria'><span id='ForSyria' class='subHeader projectTitle'><a href='#'>For Syria</a></span><span id='FSDesc' class='subHeader projectTitle workDescription'>Peace through pieces</span><br></div>");
+	// 		$('#preZween').append( "<div class='Zween'><span id='Zween' class='subHeader projectTitle'><a href='#''>Zween</a></span><span id='ZDesc' class='subHeader projectTitle workDescription'>Moroccan wine with flair</span><br></div>");		
+	// 	}
+	// });
+
+	// $('#artdirectionClick').click(function(){
+	// 		checkbox = $('#artdirectionSelected');
+
+	// 	if(checkbox.css('color') === emptyCheckbox) {
+	// 		checkbox.css('color', fullCheckbox);
+	// 		$('.ForSyria').remove();
+	// 		$('.Zween').remove();
+	// 	} else {
+	// 		checkbox.css('color', emptyCheckbox);
+	// 		$('#preForSyria').append( "<div class='ForSyria'><span id='ForSyria' class='subHeader projectTitle'><a href='#'>For Syria</a></span><span id='FSDesc' class='subHeader projectTitle workDescription'>Peace through pieces</span><br></div>");
+	// 		$('#preZween').append( "<div class='Zween'><span id='Zween' class='subHeader projectTitle'><a href='#''>Zween</a></span><span id='ZDesc' class='subHeader projectTitle workDescription'>Moroccan wine with flair</span><br></div>");		
+	// 	}
+	// });
 
 
 // SLIDER
